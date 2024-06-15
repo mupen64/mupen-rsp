ef0200a7010e0a7871c89d24b582a6d5bde9a84e
Aurumaker72 <armkdev72@gmail.com>
2024-06-16 00:01:08 +0200

    Clean up config UI

--- winproject/resource.h
+++ winproject/resource.h
@@ -1,5 +1,7 @@
-#define IDC_STATIC -1
-
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch res.rc
+//
 #define IDD_RSPCONFIG                  100
 #define IDC_ALISTS_INSIDE_RSP          101
 #define IDC_ALISTS_EMU_DEFINED_PLUGIN  102
